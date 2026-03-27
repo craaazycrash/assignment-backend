cf6b899e8cdf0015e9f6c43f80c1c89e9783b8d4
craaazycrash <craaazycrash@gmail.com>
2026-03-27 10:54:42 +0530

    first commit

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config(); // ✅ VERY IMPORTANT
+require("dotenv").config(); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// ✅ CONNECT TO MONGODB ATLAS
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Atlas Connected ✅"))
+.then(() => console.log("MongoDB Atlas Connected"))
 .catch((err) => console.log(err));
 
-// Routes
 app.use("/", require("./server/routes/routes"));
 
 app.listen(process.env.PORT || 10000, () => {
